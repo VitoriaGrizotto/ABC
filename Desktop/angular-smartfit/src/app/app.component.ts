69ee48a75fc689b96f51461aca77b94d9fea333f
Anasutero <anamaria.sutero.10@gmail.com>
2024-06-19 10:57:29 -0300

    Atualizacoes da smartfit

---
+++ Desktop/angular-smartfit/src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FormsComponent } from './components/forms/forms.component';
+
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, FormsComponent,CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+}
+)
+
+
+
+export class AppComponent {
+  title = 'desafio-smartfit';
+}
---
+++ Desktop/angular-smartfit/src/app/components/forms/forms.component.ts
@@ -0,0 +1,34 @@
+import { Component ,OnInit} from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-forms',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  templateUrl: './forms.component.html',
+  styleUrl: './forms.component.scss'
+})
+export class FormsComponent  implements OnInit {
+  results = [];
+  formGroup!: FormGroup;
+
+  constructor (private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void{
+    this.formGroup = this.formBuilder.group({
+      hour: '',
+      showClosed: false
+    })
+
+  }
+
+  onSubmit(): void{
+    console.log(this.formGroup.value) 
+  }
+
+  onClean(): void{
+   this.formGroup.reset();
+  }
+
+
+}
